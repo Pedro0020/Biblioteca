f69bbe872000b24f144f6f02a22aa0fec1f05f94
Pedro Ogando <pedromaroga@gmail.com>
2023-03-08 15:25:34 +0100

    se termina test BibliotecaLoaderTest

--- test/mibiblio/BibliotecaLoaderTest.java
+++ test/mibiblio/BibliotecaLoaderTest.java
@@ -34,24 +34,40 @@ public class BibliotecaLoaderTest {
 
     @Test
     public void testGetWindowsBaseLibros() {
-
+        if (System.getProperty("os.name").equals("Windows")) {
+            assertEquals(143, BibliotecaLoader.getWindowsBaseLibros().size());
+        } else {
+            System.err.println("La prueba testGetWindowsBaseLibros() solo se ejecuta con un sistema Windows. Ahora mismo esta usando " + System.getProperty("os.name"));
+        }
     }
 
     @Test
     public void testGetLinuxBaseLibros() {
-//                assertEquals(1, BibliotecaLoader.getLinuxBaseLibros());
-
+        if (System.getProperty("os.name").equals("Linux")) {
+            assertEquals(143, BibliotecaLoader.getLinuxBaseLibros().size());
+        } else {
+            System.err.println("La prueba testGetLinuxBaseLibros() solo se ejecuta con un sistema Linux. Ahora mismo esta usando " + System.getProperty("os.name"));
+        }
     }
 
     @Test
     public void testGetWindowsUsersAsAutores() {
-
+        if (System.getProperty("os.name").equals("Windows")) {
+            assertTrue(BibliotecaLoader.getWindowsUsersAsAutores().contains(new Autor(System.getProperty("user.name"), "Usuario-windows")));
+        } else {
+            System.err.println("La prueba testGetWindowsUsersAsAutores() solo se ejecuta con un sistema Windows. Ahora mismo esta usando " + System.getProperty("os.name"));
+        }
     }
 
     @Test
     public void testGetLinuxUsersAsAutores() {
-        assertTrue(BibliotecaLoader.getLinuxUsersAsAutores().contains(System.getProperties().));
+        if (System.getProperty("os.name").equals("Linux")) {
+            assertTrue(BibliotecaLoader.getLinuxUsersAsAutores().contains(new Autor(System.getProperty("user.name"), "Usuario-linux")));
+            assertFalse(BibliotecaLoader.getLinuxUsersAsAutores().contains(new Autor(System.getProperty("user.name"), "Usurio-linux")));
+        } else {
+            System.err.println("La prueba testGetLinuxUsersAsAutores() solo se ejecuta con un sistema Linux. Ahora mismo esta usando " + System.getProperty("os.name"));
+
+        }
 
     }
-
 }
